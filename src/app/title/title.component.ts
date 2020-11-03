d57b3d216860afeb86c2e52a2efa55acf236228c
Juan Camilo <juanccamachob94@gmail.com>
2020-11-03 09:05:01 -0600

    ng on init

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'title',
@@ -8,6 +8,16 @@ export class TitleComponent {
   public name : string = 'Juan';
   public edad : number;
   public edades : number[];
+
+  constructor() {
+    // this.name = 'Juan';
+    // this.edad = 20;
+  }
+
+  ngOnInit() {
+    this.name = 'Juan';
+    this.edad = 20;
+  }
 
   getEdadNombre() : string {
     return `${this.name} : ${this.edad}`
