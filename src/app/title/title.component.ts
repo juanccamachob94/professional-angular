85f1e354aa9e91fd9aead6031a1110fa1be0dd6e
Juan Camilo <juanccamachob94@gmail.com>
2020-11-06 15:06:44 -0600

    json and array

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -12,6 +12,7 @@ export class TitleComponent {
   public edad : number;
   public edades : number[];
   public isAvailable :boolean = true;
+  public css_classes :string[] = ['active', 'shadow'];
 
   constructor() {
     // this.name = 'Juan';
@@ -22,8 +23,9 @@ export class TitleComponent {
     this.name = 'Juan';
     this.edad = 20;
 
-    setTimeout(() => this.name = 'pruebax', 3000)
-    setTimeout(() => this.isAvailable = false, 3000)
+    setTimeout(() => this.name = 'pruebax', 3000);
+    setTimeout(() => this.isAvailable = false, 3000);
+    setTimeout(() => this.css_classes = ['shadow'], 3000);
   }
 
   getEdadNombre() : string {
